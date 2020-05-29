935ca2c4212a13319d2e96d47977e486072aacf5
eva <eva@eva.com>
2020-05-29 18:18:37 +0800

    ADD test

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/Header/Header";
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Header/>
+        <p>Such wow. Very React.</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,9 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Button from 'antd/lib/button'
-import 'antd/lib/button/style/css'
-import { Link } from 'gatsby'
+import React from "react"
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <div>
-        <div align="center">
-          <br />
-          <p
-            style={{
-              color: 'cornflowerblue',
-              fontSize: 50,
-              fontWeight: 'bold',
-            }}
-          >
-            Gatsby Markdown Starter
-          </p>
-          <h2>Boilerplate for markdown-based website</h2>
-          <br />
-          <Link to="/docs/get-started/introduction">
-            <Button
-              type="primary"
-              size="large"
-              icon="right-circle"
-              style={{ marginRight: 10 }}
-            >
-              Get Started
-            </Button>
-          </Link>
-          <Button
-            type="primary"
-            size="large"
-            icon="github"
-            href="https://github.com/cvluca/gatsby-starter-markdown"
-          >
-            Github
-          </Button>
-        </div>
-      </div>
-    </Layout>
-  )
-}
-
-export default IndexPage
+export default () => (
+    <div style={{ color: `purple` }}>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+)
